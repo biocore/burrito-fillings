ce4bd3a9c7c54c5d4fa138ea4be755aa6c80ef9f
Jenya Kopylova <jenya.kopylov@gmail.com>
2014-06-24 14:52:40 -0600

    pep8 and coverage ran on code, changes made

--- brokit/sumaclust_v1.py
+++ brokit/sumaclust_v1.py
@@ -12,35 +12,46 @@ Application controller for SumaClust version 1.0
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from os.path import split, splitext, isdir, exists, basename, dirname
-from shutil import copy, move
+from os.path import split, splitext, isdir, dirname, isfile
+
 from skbio.app.util import CommandLineApplication, ResultPath
-from skbio.app.parameters import ValuedParameter, FlagParameter, FilePath
-from skbio.parse.sequences import parse_fasta
+from skbio.app.parameters import ValuedParameter, FlagParameter
+
 
 class Sumaclust(CommandLineApplication):
-    """ SumaClust generic application controller for de novo OTU picking    
+    """ SumaClust generic application controller for de novo OTU picking
     """
 
     _command = 'sumaclust'
     _command_delimiter = ' '
     _parameters = {
-    	# Reference sequence length is the shortest
-    	'-l': FlagParameter('-', Name='l', Value=True),
-    	# Filepath of the OTU-map
-    	'-O': ValuedParameter('-', Name='O', Delimiter=' ', Value=None, IsPath=True),
+        # Reference sequence length is the shortest
+        '-l': FlagParameter('-', Name='l', Value=True),
+
+        # Filepath of the OTU-map
+        '-O': ValuedParameter('-', Name='O', Delimiter=' ',
+                              Value=None, IsPath=True),
+
         # FASTA output file is deactivated (not used in pick_otus)
         '-f': FlagParameter('-', Name='f', Value=True),
-    	# Number of threads
-    	'-p': ValuedParameter('-', Name='p', Delimiter=' ', Value=1, IsPath=False),
-    	# Assign sequence to the best matching cluster seed, rather than the first
-    	# matching cluster (having >= similarity threshold)
-    	'-e': FlagParameter('-', Name='e', Value=False),
-    	# Similarity threshold
-    	'-t': ValuedParameter('-', Name='t', Delimiter=' ', Value=0.97, IsPath=False),
-    	# Maximum ratio between abundance of two sequences so that the less abundant
-    	# one can be considered as a variant of the more abundant one. 
-    	'-R': ValuedParameter('-', Name='R', Delimiter=' ', Value=1, IsPath=False)
+
+        # Number of threads
+        '-p': ValuedParameter('-', Name='p', Delimiter=' ',
+                              Value=1, IsPath=False),
+
+        # Assign sequence to the best matching cluster seed, rather
+        # than the first matching cluster (having >= similarity threshold)
+        '-e': FlagParameter('-', Name='e', Value=False),
+
+        # Similarity threshold
+        '-t': ValuedParameter('-', Name='t', Delimiter=' ',
+                              Value=0.97, IsPath=False),
+
+        # Maximum ratio between abundance of two sequences so that the
+        # less abundant one can be considered as a variant of the more
+        # abundant one.
+        '-R': ValuedParameter('-', Name='R', Delimiter=' ',
+                              Value=1, IsPath=False)
     }
 
     _synonyms = {}
@@ -48,128 +59,104 @@ class Sumaclust(CommandLineApplication):
     _supress_stdout = False
     _supress_stderr = False
 
-    def _get_result_paths(self,data):
-    	""" Set the result paths
-    	"""
+    def _get_result_paths(self, data):
+        """ Set the result paths
+        """
 
-    	result = {}
+        result = {}
 
-    	# OTU map (mandatory output)
-    	result['OtuMap'] = ResultPath(Path=self.Parameters['-O'].Value,IsWritten=True)
+        # OTU map (mandatory output)
+        result['OtuMap'] = ResultPath(Path=self.Parameters['-O'].Value,
+                                      IsWritten=True)
 
-    	# SumaClust will not produce any output file if the 
-    	# input file was empty, so we create an empty
-    	# output file
-    	if isfile(result['OtuMap'].Path) is False:
-    		otumap_f = open(result['OtuMap'].Path, 'w')
-    		otumap_f.close()
+        # SumaClust will not produce any output file if the
+        # input file was empty, so we create an empty
+        # output file
+        if isfile(result['OtuMap'].Path) is False:
+            otumap_f = open(result['OtuMap'].Path, 'w')
+            otumap_f.close()
 
-    	return result
+        return result
 
-	def getHelp(self):
-		""" Method that points to documentation
-		"""
-		help_str =\
-		"""
-		SumaClust is hosted at:
-		http://metabarcoding.org/sumatra/
+    def getHelp(self):
+        """ Method that points to documentation
+        """
+        help_str = ("SumaClust is hosted at:\n"
+                    "http://metabarcoding.org/sumatra/\n\n"
+                    "The following paper should be cited if this resource "
+                    "is used:\n\n"
+                    "SUMATRA and SUMACLUST: fast and exact comparison and "
+                    "clustering "
+                    "of full-length barcode sequences\n"
+                    "Mercier, C., Boyer, F., Taberlet, P., Bonin, A., "
+                    "Kopylova, E. and Coissac E.,"
+                    "2014 (in preparation)\n"
+                    )
 
-		The following paper should be cited if this resource is used:
+        return help_str
 
-		"SUMATRA and SUMACLUST: fast and exact comparison and clustering of full-length barcode sequences",
-		Mercier, C., Boyer, F., Taberlet, P., Bonin, A., Kopylova, E. and Coissac E.,
-		2014 (in preparation)
-
-		"""
-
-		return help_str
 
 def sumaclust_denovo_cluster(seq_path=None,
-							result_path=None,
-							shortest_len=None,
-							similarity=None,
-							threads=1,
-							exact=None,
-							HALT_EXEC=False
-							):
-	""" Function  : launch SumaClust de novo OTU picker
+                             result_path=None,
+                             shortest_len=None,
+                             similarity=None,
+                             threads=1,
+                             exact=None,
+                             HALT_EXEC=False
+                             ):
+    """ Function  : launch SumaClust de novo OTU picker
 
-		Parameters: seq_path, filepath to reads;
-					result_path, filepath to output OTU map;
-					shortest_len, boolean;
-					similarity, the similarity threshold (between (0,1]);
-					threads, number of threads to use;
-					exact, boolean to perform exact matching
+        Parameters: seq_path, filepath to reads;
+                    result_path, filepath to output OTU map;
+                    shortest_len, boolean;
+                    similarity, the similarity threshold (between (0,1]);
+                    threads, number of threads to use;
+                    exact, boolean to perform exact matching
 
-		Return    : a dictionary of all output files set in 
-		            _get_result_paths() with the file 
-		            descriptors pointing to an open file
-		            as the values
-	"""
+        Return    : a dictionary of all output files set in
+                    _get_result_paths() with the file
+                    descriptors pointing to an open file
+                    as the values
+    """
 
-	# Instantiate the object
-	sumaclust = Sumaclust(HALT_EXEC=HALT_EXEC)
+    # Instantiate the object
+    sumaclust = Sumaclust(HALT_EXEC=HALT_EXEC)
 
-	# Set the OTU-map filepath
-	output_dir = dirname(result_path)
-	if isdir(output_dir):
-		sumaclust.Parameters['-O'].on(result_path)
-	else:
-		raise ValueError("The output directory %s does not exist." % output_dir)
+    # Set the OTU-map filepath
+    output_dir = dirname(result_path)
+    if isdir(output_dir):
+        sumaclust.Parameters['-O'].on(result_path)
+    else:
+        raise ValueError("The output directory %s does not exist."
+                         % output_dir)
 
-	# Set the similarity threshold
-	if similarity is not None:
-		sumaclust.Parameters['-t'].on(similarity)
+    # Set the similarity threshold
+    if similarity is not None:
+        sumaclust.Parameters['-t'].on(similarity)
 
-	# Set the option to perform exact clustering (default: False)
-	if exact is True:
-		sumaclust.Parameters['-e'].on()
+    # Set the option to perform exact clustering (default: False)
+    if exact is True:
+        sumaclust.Parameters['-e'].on()
 
-	# Turn off option for reference sequence length to be the shortest
-	if shortest_len is False:
-		sumaclust.Parameters['-l'].off()
+    # Turn off option for reference sequence length to be the shortest
+    if shortest_len is False:
+        sumaclust.Parameters['-l'].off()
 
-	# Set the number of threads
-	if threads is not 1:
-		sumaclust.Parameters['-p'].on(threads)
+    # Set the number of threads
+    if threads is not 1:
+        sumaclust.Parameters['-p'].on(threads)
 
-	# Launch SumaClust,
-	# set the data string to include the read filepath
-	# (to be passed as final arguments in the sumaclust command)
-	app_result = sumaclust(seq_path)
+    # Launch SumaClust,
+    # set the data string to include the read filepath
+    # (to be passed as final arguments in the sumaclust command)
+    app_result = sumaclust(seq_path)
 
-	# Put clusters into a list of lists
-	clusters = []
-	f_otumap = app_result['OtuMap']
-	for line in f_otumap:	
-		cluster = line.strip().split('\t')
-		clusters.append(cluster[1:])
+    # Put clusters into a list of lists
+    clusters = []
+    f_otumap = app_result['OtuMap']
+    for line in f_otumap:
+        cluster = line.strip().split('\t')
+        clusters.append(cluster[1:])
 
-	# Return clusters 
-	return clusters
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Return clusters
+    return clusters
--- brokit/tests/test_sumaclust_v1.py
+++ brokit/tests/test_sumaclust_v1.py
@@ -6,15 +6,17 @@ Unit tests for the SumaClust version 1.0 Application controller
 
 
 from unittest import TestCase, main
-from brokit.sumaclust_v1 import (Sumaclust,
-								 sumaclust_denovo_cluster)
-from tempfile import mkstemp, mkdtemp
-from skbio.util.misc import remove_files
-from os import close
-from os.path import abspath, exists, getsize
-from skbio.parse.sequences import parse_fasta
 import re
 import filecmp
+from tempfile import mkstemp
+from os import close
+from os.path import exists, getsize
+
+from skbio.util.misc import remove_files
+from skbio.parse.sequences import parse_fasta
+
+from brokit.sumaclust_v1 import (Sumaclust,
+                                 sumaclust_denovo_cluster)
 
 
 # ----------------------------------------------------------------------------
@@ -28,67 +30,67 @@ import filecmp
 
 # Reads to cluster
 # there are 30 reads representing 3 species (gives 3 clusters)
-reads_seqs = """>s1_630 reference=1049393 amplicon=complement(497..788) position=complement(193..292)
+reads_seqs = """>s1_630 reference=1049393 amplicon=complement(497..788)
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_2369 reference=1049393 amplicon=complement(497..788) position=complement(193..292) errors=73%A
+>s1_2369 reference=1049393 amplicon=complement(497..788) errors=73%A
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTAGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_3750 reference=1049393 amplicon=complement(497..788) position=complement(193..292) errors=100%A
+>s1_3750 reference=1049393 amplicon=complement(497..788) errors=100%A
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCA
->s1_4572 reference=1049393 amplicon=complement(497..788) position=complement(193..292)
+>s1_4572 reference=1049393 amplicon=complement(497..788)
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_5748 reference=1049393 amplicon=complement(497..788) position=complement(193..292)
+>s1_5748 reference=1049393 amplicon=complement(497..788)
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_6846 reference=1049393 amplicon=complement(497..788) position=complement(193..292) errors=67%A
+>s1_6846 reference=1049393 amplicon=complement(497..788) errors=67%A
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCATAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_7634 reference=1049393 amplicon=complement(497..788) position=complement(193..292) errors=99%T
+>s1_7634 reference=1049393 amplicon=complement(497..788) errors=99%T
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTTG
->s1_8623 reference=1049393 amplicon=complement(497..788) position=complement(193..292) errors=17-
+>s1_8623 reference=1049393 amplicon=complement(497..788) errors=17-
 GTGCCAGCAGCCGCGGAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_8744 reference=1049393 amplicon=complement(497..788) position=complement(193..292) errors=62%A
+>s1_8744 reference=1049393 amplicon=complement(497..788) errors=62%A
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGAGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_13961 reference=1049393 amplicon=complement(497..788) position=complement(193..292)
+>s1_13961 reference=1049393 amplicon=complement(497..788)
 GTGCCAGCAGCCGCGGTAATACAGAGGTCTCAAGCGTTGTTCGGATTCATTGGGCGTAAAGGGTGCGTAGGTGGCGGGGTAAGTCAGGTGTGAAATCTCG
->s1_4677 reference=4382408 amplicon=complement(487..778) position=complement(193..292) errors=74%T
+>s1_4677 reference=4382408 amplicon=complement(487..778) errors=74%T
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGTGTCTGTAAGTCAGAGGTGAAAGCCCA
->s1_8592 reference=4382408 amplicon=complement(487..778) position=complement(193..292) errors=95+A
+>s1_8592 reference=4382408 amplicon=complement(487..778) errors=95+A
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGAAAAGCCCA
->s1_8977 reference=4382408 amplicon=complement(487..778) position=complement(193..292)
+>s1_8977 reference=4382408 amplicon=complement(487..778)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGAAAGCCCA
->s1_10439 reference=4382408 amplicon=complement(487..778) position=complement(193..292)
+>s1_10439 reference=4382408 amplicon=complement(487..778)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGAAAGCCCA
->s1_11001 reference=4382408 amplicon=complement(487..778) position=complement(193..292) errors=91%G
+>s1_11001 reference=4382408 amplicon=complement(487..778) errors=91%G
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGGGAAAGCCCA
->s1_11650 reference=4382408 amplicon=complement(487..778) position=complement(193..292) errors=78-
+>s1_11650 reference=4382408 amplicon=complement(487..778) errors=78-
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCGTAAGTCAGAGGTGAAAGCCCA
->s1_12366 reference=4382408 amplicon=complement(487..778) position=complement(193..292)
+>s1_12366 reference=4382408 amplicon=complement(487..778)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGAAAGCCCA
->s1_14735 reference=4382408 amplicon=complement(487..778) position=complement(193..292) errors=94%C
+>s1_14735 reference=4382408 amplicon=complement(487..778) errors=94%C
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGACAGCCCA
->s1_15985 reference=4382408 amplicon=complement(487..778) position=complement(193..292)
+>s1_15985 reference=4382408 amplicon=complement(487..778)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGAAAGCCCA
->s1_21935 reference=4382408 amplicon=complement(487..778) position=complement(193..292)
+>s1_21935 reference=4382408 amplicon=complement(487..778)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTCCAAGCGTTGTCCGGAATCACTGGGTGTAAAGGGTGCGTAGGCGGGTCTGTAAGTCAGAGGTGAAAGCCCA
->s1_844 reference=129416 amplicon=complement(522..813) position=complement(193..292)
+>s1_844 reference=129416 amplicon=complement(522..813)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_1271 reference=129416 amplicon=complement(522..813) position=complement(193..292) errors=94%C
+>s1_1271 reference=129416 amplicon=complement(522..813) errors=94%C
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGACAGCCCA
->s1_1886 reference=129416 amplicon=complement(522..813) position=complement(193..292)
+>s1_1886 reference=129416 amplicon=complement(522..813)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_5347 reference=129416 amplicon=complement(522..813) position=complement(193..292)
+>s1_5347 reference=129416 amplicon=complement(522..813)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_5737 reference=129416 amplicon=complement(522..813) position=complement(193..292)
+>s1_5737 reference=129416 amplicon=complement(522..813)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_6200 reference=129416 amplicon=complement(522..813) position=complement(193..292) errors=92%C
+>s1_6200 reference=129416 amplicon=complement(522..813) errors=92%C
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTCAAAGCCCA
->s1_7014 reference=129416 amplicon=complement(522..813) position=complement(193..292)
+>s1_7014 reference=129416 amplicon=complement(522..813)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_7040 reference=129416 amplicon=complement(522..813) position=complement(193..292) errors=40%G
+>s1_7040 reference=129416 amplicon=complement(522..813) errors=40%G
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTAGTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_7881 reference=129416 amplicon=complement(522..813) position=complement(193..292)
+>s1_7881 reference=129416 amplicon=complement(522..813)
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTAAGTCAGATGTGAAAGCCCA
->s1_8615 reference=129416 amplicon=complement(522..813) position=complement(193..292) errors=81%G
+>s1_8615 reference=129416 amplicon=complement(522..813) errors=81%G
 GTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTATTCGGAATTACTGGGCGTAAAGGGCGTGTAGGCGGCTTTGTGAGTCAGATGTGAAAGCCCA
-""" 
+"""
 
 # The expected OTU map from clustering the read_seqs from 3 species
 expected_otumap = """s1_844\ts1_844\ts1_1886\ts1_5347\ts1_5737\ts1_7014\ts1_7881\ts1_7040\ts1_6200\ts1_1271\ts1_8615
@@ -96,87 +98,86 @@ s1_8977\ts1_8977\ts1_10439\ts1_12366\ts1_15985\ts1_21935\ts1_11650\ts1_11001\ts1
 s1_13961\ts1_13961\ts1_4572\ts1_5748\ts1_630\ts1_2369\ts1_3750\ts1_7634\ts1_8623\ts1_8744\ts1_6846
 """
 
+
 class SumaclustV1Tests(TestCase):
-	""" Tests for Sumaclust version 2.0 functionality """
+    """ Tests for Sumaclust version 2.0 functionality """
 
-	def setUp(self):
+    def setUp(self):
 
-		self.output_dir = '/tmp/'
-		self.read_seqs = reads_seqs
-		self.expected_otumap = expected_otumap
+        self.output_dir = '/tmp/'
+        self.read_seqs = reads_seqs
+        self.expected_otumap = expected_otumap
 
-		# create temporary file with read sequences defined in read_seqs
-		f, self.file_read_seqs = mkstemp(prefix='temp_reads_',
-										 suffix='.fasta')
-		close(f)
+        # create temporary file with read sequences defined in read_seqs
+        f, self.file_read_seqs = mkstemp(prefix='temp_reads_',
+                                         suffix='.fasta')
+        close(f)
 
-		# write read sequences to tmp file
-		tmp = open(self.file_read_seqs, 'w')
-		tmp.write(self.read_seqs)
-		tmp.close()
+        # write read sequences to tmp file
+        tmp = open(self.file_read_seqs, 'w')
+        tmp.write(self.read_seqs)
+        tmp.close()
 
-		# create temporary file with final OTU map
-		f, self.file_otumap = mkstemp(prefix='temp_otumap',
-										suffix='.txt')
-		close(f)
+        # create temporary file with final OTU map
+        f, self.file_otumap = mkstemp(prefix='temp_otumap',
+                                      suffix='.txt')
+        close(f)
 
-		# write OTU map to tmp file
-		tmp = open(self.file_otumap, 'w')
-		tmp.write(self.expected_otumap)
-		tmp.close()
+        # write OTU map to tmp file
+        tmp = open(self.file_otumap, 'w')
+        tmp.write(self.expected_otumap)
+        tmp.close()
 
-		# list of files to remove
-		self.files_to_remove = [self.file_read_seqs, self.file_otumap]
+        # list of files to remove
+        self.files_to_remove = [self.file_read_seqs, self.file_otumap]
 
-	def tearDown(self):
-		pass
-		#remove_files(self.files_to_remove)
+    def tearDown(self):
+        remove_files(self.files_to_remove)
 
-	def test_sumaclust_denovo_cluster(self):
-		""" Test de novo clustering with SumaClust """
+    def test_sumaclust_denovo_cluster(self):
+        """ Test de novo clustering with SumaClust """
 
-		result_path = self.output_dir + "sumaclust_otus.txt"
+        result_path = self.output_dir + "sumaclust_otus.txt"
 
-		clusters = sumaclust_denovo_cluster(seq_path=self.file_read_seqs,
-												result_path=result_path,
-												shortest_len=True,
-												similarity=None,
-												threads=1,
-												exact=False,
-												HALT_EXEC=False)
+        clusters = sumaclust_denovo_cluster(seq_path=self.file_read_seqs,
+                                            result_path=result_path,
+                                            shortest_len=True,
+                                            similarity=None,
+                                            threads=1,
+                                            exact=False,
+                                            HALT_EXEC=False)
 
-		# Check the OTU map was output with the correct size
-		self.assertTrue(exists(result_path))
+        # Check the OTU map was output with the correct size
+        self.assertTrue(exists(result_path))
 
-		# Checkout output file has the correct size
-		size = getsize(result_path)
-		self.assertTrue(size,270)
+        # Checkout output file has the correct size
+        size = getsize(result_path)
+        self.assertTrue(size, 270)
 
-		# Check file identical to expected result (the hash should be identical)
-		self.assertTrue(filecmp.cmp(result_path,self.file_otumap))
+        # Check file identical to expected result (the hash should
+        # be identical)
+        self.assertTrue(filecmp.cmp(result_path, self.file_otumap))
 
-		# Check the returned clusters list of lists is as expected
-		expected_clusters = [['s1_844', 's1_1886', 's1_5347', 's1_5737',
-							  's1_7014', 's1_7881', 's1_7040', 's1_6200',
-							  's1_1271', 's1_8615'],
-							 ['s1_8977', 's1_10439', 's1_12366', 's1_15985',
-							  's1_21935', 's1_11650', 's1_11001', 's1_8592', 
-							  's1_14735', 's1_4677'],
-							 ['s1_630', 's1_4572', 's1_5748', 's1_13961',
-							  's1_2369', 's1_3750', 's1_7634', 's1_8623',
-							  's1_8744', 's1_6846']]
+        # Check the returned clusters list of lists is as expected
+        expected_clusters = [['s1_844', 's1_1886', 's1_5347', 's1_5737',
+                              's1_7014', 's1_7881', 's1_7040', 's1_6200',
+                              's1_1271', 's1_8615'],
+                             ['s1_8977', 's1_10439', 's1_12366', 's1_15985',
+                              's1_21935', 's1_11650', 's1_11001', 's1_8592',
+                              's1_14735', 's1_4677'],
+                             ['s1_630', 's1_4572', 's1_5748', 's1_13961',
+                              's1_2369', 's1_3750', 's1_7634', 's1_8623',
+                              's1_8744', 's1_6846']]
 
-		# Should be 3 clusters
-		self.assertEqual(3,len(clusters))
-		i=0
-		for actual_cluster in clusters:
-			actual_cluster.sort()
-			expected_clusters[i].sort()
-			self.assertEqual(actual_cluster,expected_clusters[i])
-			i+=1
-
+        # Should be 3 clusters
+        self.assertEqual(3, len(clusters))
+        i = 0
+        for actual_cluster in clusters:
+            actual_cluster.sort()
+            expected_clusters[i].sort()
+            self.assertEqual(actual_cluster, expected_clusters[i])
+            i += 1
 
 
 if __name__ == '__main__':
     main()
-

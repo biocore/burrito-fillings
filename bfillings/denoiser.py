8666e71907520640e258a02324572b872438ccb4
Greg Caporaso <gregcaporaso@gmail.com>
2014-11-12 13:02:26 -0700

    MAINT: removed outdated comment

--- bfillings/denoiser.py
+++ bfillings/denoiser.py
@@ -16,9 +16,6 @@ targeted either for complete re-write or removal pending benchmarks. The basic
 idea is that it's not worth porting this code anywhere now because it's days are
 numbered, but we still need to be able to access it for the time being.
 
-May The Biological Research (application controller) Obsolescence Kit (bfillings)
-be as short-lived as its name is awesome.
-
 """
 
 from cogent.parse.flowgram import (Flowgram, build_averaged_flowgram,
